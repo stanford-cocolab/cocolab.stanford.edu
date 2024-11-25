5cdedbdda4aca4cea10b036e7e468e325f5c58c2
Kanishk Gandhi <kanishk050296@gmail.com>
2024-11-24 18:48:11 -0800

    update update script

--- update.sh
+++ update.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 WWW_DIR="/afs/ir.stanford.edu/group/cocolab/WWW/"
-HOST="juliawhi@myth.stanford.edu"
 
-set -e
-
-JEKYLL_ENV=production jekyll build
+git pull
+bundle exec jekyll build
 
 pushd _site
 rm -f site.zip
 
 zip -r site.zip *
-scp site.zip "$HOST:$WWW_DIR"
+cp site.zip $WWW_DIR
 
-ssh -t "$HOST" "cd $WWW_DIR && unzip -o site.zip && rm site.zip && rm update.sh"
+cd $WWW_DIR && unzip -o site.zip && rm site.zip
 
 popd
